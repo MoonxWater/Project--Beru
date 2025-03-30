872ab7ca328bd89cfe8d246e671d0087a568a45c
Moon Water <moonxwaterx@gmail.com>
2025-03-30 21:36:37 +0530

    modified search capabilities, changed kill word, added descriptive comments to functions

--- main.py
+++ main.py
@@ -10,14 +10,13 @@ engine = pyttsx3.init()
 
 wake = False # the assistant will not be wake at program initialization and you would need to use the wake up word and that will update wake to True
 wake_up_word = "arise" # use this to wake assistant up
-kill_word = "kill" # use this word to exit program (the voice input should only contain kill)
-app_paths = {
-    "steam": "C:\\Program Files (x86)\\Steam\\steam.exe",
-    "epic games": "C:\\Program Files (x86)\\Epic Games\\Launcher\\Portal\\Binaries\\Win32\\EpicGamesLauncher.exe",
-    "code": os.path.expandvars("%LOCALAPPDATA%\\Programs\\Microsoft VS Code\\Code.exe"),
-    "brave": os.path.expandvars("%LOCALAPPDATA%\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"),
-    "opera": os.path.expandvars("%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Opera GX Browser.lnk"),
-    } # if you want to add an app to be opened by the assistant, add its path to value and word to trigger opening in the key of app_paths dict
+kill_word = "kill program" # use this word to exit program (the voice input should only contain kill)
+app_paths = {"steam": "C:\\Program Files (x86)\\Steam\\steam.exe",
+            "epic games": "C:\\Program Files (x86)\\Epic Games\\Launcher\\Portal\\Binaries\\Win32\\EpicGamesLauncher.exe",
+            "code": os.path.expandvars("%LOCALAPPDATA%\\Programs\\Microsoft VS Code\\Code.exe"),
+            "brave": os.path.expandvars("%LOCALAPPDATA%\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"),
+            "opera": os.path.expandvars("%APPDATA%\\Microsoft\\Windows\\Start Menu\\Programs\\Opera GX Browser.lnk"),
+            } # if you want to add an app to be opened by the assistant, add its path to value and word to trigger opening in the key of app_paths dict
 websites: dict = {'hi anime': 'https://hianime.is',
                   'ashura': 'https://asuracomic.net/',
                   'reaper': 'https://reaperscans.com',
@@ -30,10 +29,10 @@ websites: dict = {'hi anime': 'https://hianime.is',
 
 def speak(text: str) -> None:
     """
-    This function converts provided text to speech
+    Converts text to speech
 
     :param (text): The text that you want the engine to speak should be provided at call
-    :return: return None
+    :return: returns None
     """
     print(text)
     engine.say(text)
@@ -42,32 +41,32 @@ def speak(text: str) -> None:
 
 def process_command(c: str) -> None:
     """
-    This function takes input and completes action provided in the command
+    Processes user voice commands
 
     :param (c): command to be provided at function call
     :return: returns None
     """
 
-    c = c.lower()
+    global wake
     app_dir = app_paths
     web_url = websites
 
-    if 'sleep' in c.lower().strip():
+    if c.startswith('sleep'):
         # updates wake to False and returns to the point where it wakes after listening to wake word
         speak('Hibernating')
-        global wake
         wake = False
         return
     
-    elif c == kill_word: # c should only contain the kill word and nothing else
-        speak('Exit word used. Exiting program.')
+    elif c.startswith(kill_word): # "c" should ideally contain only the kill_word and nothing else
+        speak('Kill word used. Purging program.')
         exit()
     
     # commands to open
-    elif 'open' in c:
+    elif c.startswith('open'):
+        c = c.replace('open', '', count=1)
         # to open websites
         for website, url in web_url.items():
-            if f'open {website}' in c:
+            if f'{website}' in c:
                 speak(f'Opening {website}')
                 webbrowser.open(url)
                 speak('Process complete. Waiting for next command.')
@@ -75,35 +74,33 @@ def process_command(c: str) -> None:
         
         # to open apps
         for app, path in app_dir.items():
-            if f'open {app}' in c:
+            if f'{app}' in c:
                 speak(f'Opening {app}')
                 os.startfile(path)
                 speak('Process complete. Waiting for next command.')
                 return
-        
-        else:
-            speak(f'You said: {c}')
-            print(c)
 
     # command to search pre-defined platforms
-    elif 'search' in c:
+    elif c.startswith('search'):
         for platform, url in web_url.items():
             if f'{platform}' in c:
-                no_error = webbrowser.open(url)
+                no_error: bool = webbrowser.open(url)
+
                 if no_error == False: 
                     # if the open function in webbrowser module couldn't open url and returns False 
                     speak(f'Something went wrong while opening {platform}')
                     return
+                
                 time.sleep(2) # wait for url to open
 
                 while True:
                     speak(f"What would you like to search on {platform}?")
                     search = take_command('What would you like to search?', 3, 3, wake)
 
-                    speak(f'Do you want to search for: {search}, yes or no or quit')
-                    confirmation = take_command("Confirm(Yes/No)", 2, 1, wake).lower().strip()
+                    speak(f'Do you want to search for: {search}, ok or no or quit')
+                    confirmation = take_command("Confirm(ok/No)", 2, 2, wake)
 
-                    if 'yes' in confirmation:
+                    if 'okay' in confirmation or 'ok' in confirmation:
                         if f'{platform}' == 'youtube':
                             pyautogui.click(900, 150)
 
@@ -125,10 +122,17 @@ def process_command(c: str) -> None:
 
 
 
-def take_command(text, timeout: int = None, phrase_time_limit: int = None, wake:bool = False):
+def take_command(text, timeout: int = None, phrase_time_limit: float = None, wake:bool = False) -> str:
+    """
+    :param text: will print text before it starts to listen
+    :param timeout: int to provide timeout to listen function
+    :param phrase_time_limit: int to provide phrase_time_limit to listen function
+    :param wake: bool to speak beep before listening
+    :return: str after successfully recognizing.
+    
+    """
     while True:
         # Listen for command
-        print(text)
         try:
             with sr.Microphone() as source:
                 print('Listening...')
@@ -136,7 +140,7 @@ def take_command(text, timeout: int = None, phrase_time_limit: int = None, wake:
                     speak('Beep')
                 audio = recogniser.listen(source, timeout=timeout, phrase_time_limit=phrase_time_limit)
             print('Recognizing...\n')
-            return recogniser.recognize_google(audio)
+            return str(recogniser.recognize_google(audio)).lower().strip()
 
         except sr.UnknownValueError:
             print('Couldn\'t catch that...')
@@ -156,21 +160,21 @@ def main():
     speak('Initializing')        
 
     while True:
-        word = take_command("Initialize by wake up word...", 2, 2, wake)
+        word = take_command("Initialize by wake word...", 2, 2, wake)
         print(word)
         
-        if(wake_up_word in word.lower()):
+        if(wake_up_word in word):
             wake = True
             speak("At your command.")
             while wake == True:
                 command = take_command("Jarvis Active...", 2, 3, wake)
                 process_command(command)
                 
-        elif(kill_word in word.lower()):
-            speak('Killing Program.')
+        elif kill_word in word:
+            speak('Purging Process.')
             break
 
-        elif('help') in word.lower():
+        elif 'help' in word:
             text = (f'You can wake program by using "{wake_up_word}" or kill program by using "{kill_word}".')
             print(text)
             speak(text)
